ff47895865a2076a583994a8f8c6903e5955d637
camer <camer@10.113.79.36>
2016-08-17 22:00:44 -0300

    gonna use modals for todos estos uuurgh

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if(isset($_GET['key'])){
     </head>
     <body>
         
-        <nav class=''>
+        <nav>
             <div class="nav-wrapper container white-text" >
                 <div class="brand-logo">
                     <!-- <img src="res/media_reverse.png" /> -->
@@ -45,31 +45,41 @@ if(isset($_GET['key'])){
         </nav>
         
         <main id="main">
-            <div class="row">
-                <div class="col s12 m8 l9">
-                    <div class="container">
-                    <button class="btn" onclick="$('#filemodal').openModal();">Add files</button>
+            <div class="container">
+                <button class="btn" id="uploadbtn" onclick="$('#uploadmodal').openModal();">Add files</button>
 
-                    <?php
-                    foreach(file_list::get_all() as $file){
-                        $id = $file->properties->id;
-                        echo "<div onclick=\"fileinfo.open({$id});\">";
-                        echo "<img width=\"150\" class=\"not-materialboxed\" src=\"files/{$id}/{$file->properties->thumbnail}\"/>";
-                        echo "<p>{$file->properties->name}</p>";
-                        echo "</div>";
-                    }
-                    ?>
-                    </div>
-                </div>
-                <div class="col s12 m4 l3">
-                    <div class="container">
-                        <h4>Upload a file</h4>
-                    </div>
-                </div>
+                <?php
+                foreach(file_list::get_all() as $file){
+                    $id = $file->properties->id;
+                    echo "<div onclick=\"fileinfo.open({$id});\">";
+                    echo "<img width=\"150\" class=\"not-materialboxed\" src=\"files/{$id}/{$file->properties->thumbnail}\"/>";
+                    echo "<p>{$file->properties->name}</p>";
+                    echo "</div>";
+                }
+                ?>
             </div>
         </main>
         
-        <div id="filemodal" class="modal bottom-sheet">
+        <div id="uploadmodal" class="modal bottom-sheet">
+            <div class="modal-content">
+                <form action="#">
+                    <div class="file-field input-field">    
+                        <div class="btn">
+                            <span>File</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Upload</a>
+            </div>
+        </div>
+        
+        <div id="detailsmodal" class="modal bottom-sheet">
             <div class="modal-content">
                 <form action="#">
                     <div class="file-field input-field">    
@@ -93,6 +103,9 @@ if(isset($_GET['key'])){
         <!--Import jQuery before materialize.js-->
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script>
+            $(".button-collapse").sideNav();
+        </script>
     </body>
 </html>
 
